c128352d9c2da5780464c7297394d199d96a0645
anurag-040 <anuragsyadav04@gmail.com>
2023-10-16 12:05:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Search from './components/search/search';
+import Forecast from './components/forecast/forecast';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
+import { useState } from "react";
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleonSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({city: searchData.label, ...weatherResponse});
+        setForecast({city: searchData.label, ...forecastResponse});
+      })
+      .catch((err)=>{console.log(err)})
+  }
+
+  console.log(forecast);
+  return (
+    <div className="container">
+      <Search onSearchChange={handleonSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9630ea0847msh345d7b979918bfep114444jsn3a6d9d7f825f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1b88d4fc678b950e45c7552257217428";
